27eac86676e070bf221eedde89bc960b958f16f5
hanmanjung <201840233@email.daelim.ac.kr>
2021-03-30 15:54:52 +0900

    hello

---
+++ hello.js
@@ -0,0 +1,30 @@
+let date = new Date();
+h = date.getHours();
+
+console.log(h < 3 || h > 8);
+console.log(h >= 3 && h <= 8);
+
+
+let type = typeof(date.getHours());
+console.log(type);
+
+const con1 = "상수선언";
+con1="";
+console.log(con1);
+
+let input = 32;
+
+if (input % 2 == 0) {
+    console.log("짝수!");
+}
+if (date.getHours() < 12) {
+    console.log("오전!");
+}
+console.log("오후!");
+
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getTime());
+console.log(date.getUTCMinutes());
